eff7fd195ce82449017c1e700ebdcfdb915eaa9d
PannProjects <pandumalang321@.com>
2025-05-07 17:43:03 +0700

    added ViewAction to UserResource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -52,6 +52,7 @@ class UserResource extends Resource
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
