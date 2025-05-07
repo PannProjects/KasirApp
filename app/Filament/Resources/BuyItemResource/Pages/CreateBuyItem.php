ca4918386e664be6da31506951f359fe60ab8476
PannProjects <pandumalang321@.com>
2025-05-07 17:44:42 +0700

    added buyItem

---
+++ app/Filament/Resources/BuyItemResource/Pages/CreateBuyItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BuyItemResource\Pages;
+
+use App\Filament\Resources\BuyItemResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBuyItem extends CreateRecord
+{
+    protected static string $resource = BuyItemResource::class;
+}
